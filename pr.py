b970c4dfe9da7c05d7ec21cb6b01418280e2c150
takonan <sika_tak@yahoo.co.jp>
2024-12-12 20:33:36 +0900

    1st commit

--- pr.py
+++ pr.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 import datetime as dt
+import math
 
 
 st.set_page_config(
     page_title="ジャンボウサイト"
 )
 
-#精度向上ボタンで役立つ関数1
-def time_check(x):
-    return (x-21.84)
-
-#精度向上ボタンで役立つ関数2
-def point_check(y):
-    return (-y+1000)/100
+##精度向上ボタンで役立つ関数1
+#def time_check(x):
+#    return (x-21.84)
+#
+##精度向上ボタンで役立つ関数2
+#def point_check(y):
+#    return (-y+1000)/100
 
 #種目選択を可能にする調整
 def reshape_recode(z):
@@ -47,26 +48,26 @@ df = pd.DataFrame({'Fr':['9.83','21.84','49.21','108.95','236.47','498.16','951.
     )
 st.dataframe(df)
 
-#50m自由形　日本記録からきれいな数字0.5秒刻みのポイント対応表
-df2 = pd.DataFrame({
-    'time':[21.84,22.00,22.25,22.50,22.75,23.00,23.50,24.00,24.50,25.00,25.50,26.00,26.50,27.00,27.50,28.00,28.50,29.00,29.50,30.00,30.50,31.00,31.25,31.50,31.75,32.00],
-    'point':[1000,978,945,914,884,856,802,753,708,666,628,592,559,529,500,474,450,427,405,385,367,349,341,333,325,317]
-})
-st.write(len(df2))
-#どうやら二次関数のようなので式を導出する
-time_ex = df2[['time']].apply(time_check)
-point_ex = df2[['point']].apply(point_check)
-
-X = pd.DataFrame(time_ex,index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
-Y = pd.DataFrame(point_ex,index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
-Z = pd.concat([X,Y],axis=1)
-
-
-
-#対応表の散布図
-st.scatter_chart(df2,x = "time",y = "point")
-#数字を整えたデータフレームとその散布図
-st.scatter_chart(Z,x = 'point',y = 'time')
+##50m自由形　日本記録からきれいな数字0.5秒刻みのポイント対応表
+#df2 = pd.DataFrame({
+#    'time':[21.84,22.00,22.25,22.50,22.75,23.00,23.50,24.00,24.50,25.00,25.50,26.00,26.50,27.00,27.50,28.00,28.50,29.00,29.50,30.00,30.50,31.00,31.25,31.50,31.75,32.00],
+#    'point':[1000,978,945,914,884,856,802,753,708,666,628,592,559,529,500,474,450,427,405,385,367,349,341,333,325,317]
+#})
+#st.write(len(df2))
+##どうやら二次関数のようなので式を導出する
+#time_ex = df2[['time']].apply(time_check)
+#point_ex = df2[['point']].apply(point_check)
+#
+#X = pd.DataFrame(time_ex,index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
+#Y = pd.DataFrame(point_ex,index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
+#Z = pd.concat([X,Y],axis=1)
+#
+#
+#
+##対応表の散布図
+#st.scatter_chart(df2,x = "time",y = "point")
+##数字を整えたデータフレームとその散布図
+#st.scatter_chart(Z,x = 'point',y = 'time')
 
 
 
@@ -95,29 +96,31 @@ elif select_recode >= 60:
     st.write('日本記録：',shape_recode_m,'分',shape_recode_s,'秒,貴方の記録：',time,' 日本記録まであと',Rest)
 
 
-#二次関数の係数を導出　y = a ** x a:の導出
-#反比例　y = a/x a:の導出
-for i in range(0,26):
-    a = round(float(X.iloc[i,0] / pow(Y.iloc[i,0],2)),3)
-    a_round = round(a,2)
-    st.write(a,a_round,round(X.iloc[i,0],2),round(Y.iloc[i,0],2))
+##二次関数の係数を導出　y = a ** x a:の導出
+##反比例　y = a/x a:の導出
+#for i in range(0,26):
+#    a = round(float(X.iloc[i,0] / pow(Y.iloc[i,0],2)),3)
+#    a_round = round(a,2)
+#    st.write(a,a_round,round(X.iloc[i,0],2),round(Y.iloc[i,0],2))
 
 
-x2 = st.number_input("pointを入力してください(50m自由形のタイムを返します)")
-y2 = (0.22*pow((-x2+1000)/100,2))
+mypoint = st.number_input("pointを入力してください")
+rtp = math.pow(mypoint/1000,1/3)
+mytime = select_recode/rtp
 #Y1 = (y2*100)+21.84
-Y1 = round((y2+21.84),2)
-st.write(Y1,"秒")
+#Y1 = round((y2+21.84),2)
+Y1 = round(mytime,2)
+st.write(distance,style,Y1,"秒")
 
-#単回帰分析結果－情報の精度か足りずいまいち
-model_lr = LinearRegression()
-model_lr.fit(Y,X)
+##単回帰分析結果－情報の精度か足りずいまいち
+#model_lr = LinearRegression()
+#model_lr.fit(Y,X)
+##
+#st.write("モデル関数の回帰変数w1:%.3f"%model_lr.coef_)
+#st.write("モデル関数の切片w2:%.3f"%model_lr.intercept_)
+#st.write("y = %.3fx+%.3f"%(model_lr.coef_,model_lr.intercept_))
 #
-st.write("モデル関数の回帰変数w1:%.3f"%model_lr.coef_)
-st.write("モデル関数の切片w2:%.3f"%model_lr.intercept_)
-st.write("y = %.3fx+%.3f"%(model_lr.coef_,model_lr.intercept_))
-
-x3 = st.number_input("point値を入力してください")
-Y2 = model_lr.coef_*((-x3+1000)/100)+model_lr.intercept_
-st.write(Y2+21.84)
+#x3 = st.number_input("point値を入力してください")
+#Y2 = model_lr.coef_*((-x3+1000)/100)+model_lr.intercept_
+#st.write(Y2+21.84)
 
